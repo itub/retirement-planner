6cb834c0e9af86226b9c7fe1d4c59263d563102d
Dan Hardy <dan.hardy@mac.lan>
2026-04-25 13:53:45 -0700

    update sameSite

--- server/src/index.js
+++ server/src/index.js
@@ -34,6 +34,8 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   },
 }));
